b9fd4bc86781a3c4790613ee8f871cdcef27bcae
Anant <anant13u@gmail.com>
2023-04-30 15:01:05 +0530

    Wrote an arrow function which will take the path of the image, and then download it after using the substring method.

--- script.js
+++ script.js
@@ -36,6 +36,21 @@
 //       });
 //   }
 
+let downloadBtn = document.querySelector('#downloadBtn');
+let myImg = document.querySelector('#firstImage');
+
+
+// Must use FileSaver.js 2.0.2 because 2.0.3 has issues.
+downloadBtn.addEventListener('click', () => {
+    let imagePath = myImg.getAttribute('src');
+    let fileName = getFileName(imagePath);
+    saveAs(imagePath, fileName);
+});
+
+function getFileName(str) {
+    return str.substring(str.lastIndexOf('/') + 1)
+}
+
 
 
 
