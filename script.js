caf10427dfdccb4121c6099adb8317948433d5b5
Anant <anant13u@gmail.com>
2023-07-12 22:45:07 +0530

    Commented out the download code which wasn't working without the download button from the HTML file.

--- script.js
+++ script.js
@@ -18,41 +18,20 @@
 //   link.click();
 // });
 
-
-
-// function downloadImage() {
-//     const imgSrc = document.querySelector('#firstImage').getAttribute('src');
-//     const imgName = imgSrc.split('/').pop();
-//     fetch(imgSrc)
-//       .then(res => res.blob())
-//       .then(blob => {
-//         const url = window.URL.createObjectURL(new Blob([blob]));
-//         const link = document.createElement('a');
-//         link.href = url;
-//         link.setAttribute('download', imgName);
-//         document.body.appendChild(link);
-//         link.click();
-//         link.parentNode.removeChild(link);
-//       });
-//   }
-
 let downloadBtn = document.querySelector('#downloadBtn');
 let myImg = document.querySelector('#firstImage');
 
 
 // Must use FileSaver.js 2.0.2 because 2.0.3 has issues.
-downloadBtn.addEventListener('click', () => {
-    let imagePath = myImg.getAttribute('src');
-    let fileName = getFileName(imagePath);
-    saveAs(imagePath, fileName);
-});
+// downloadBtn.addEventListener('click', () => {
+//     let imagePath = myImg.getAttribute('src');
+//     let fileName = getFileName(imagePath);
+//     saveAs(imagePath, fileName);
+// });
 
 function getFileName(str) {
     return str.substring(str.lastIndexOf('/') + 1)
 }
-
-
-
 
 const matterHeading = document.querySelector('.matter-heading');
 matterHeading.addEventListener('click', openGoogle);
